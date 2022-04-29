e543f60bde0e5b9b9afd13001544931920e719c7
Mahesh Chandra <mmahesh.chandra873@gmail.com>
2022-04-29 16:37:10 +0530

    added cpu

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ args = parser.parse_args()
 
 
 # # training
-trainer = pl.Trainer(accelerator='gpu', gpus=args.gpus, precision=args.precision,
+trainer = pl.Trainer(accelerator='cpu', gpus=args.gpus, precision=args.precision,
                      limit_train_batches=args.limit_train_batches)
 trainer.fit(model, train_loader, val_loader)
 
