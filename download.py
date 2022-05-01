226e6bf8894272ed2665e89852a73e0081e70d0a
Mahesh Chandra <mmahesh.chandra873@gmail.com>
2022-05-01 16:37:59 +0530

    changing download mnist

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 import torchvision as torchvision
 import torchvision.transforms as T
+import os 
 
 if __name__ == '__main__':
+    os.makedirs(f"data", exist_ok=True)
     torchvision.datasets.MNIST(
         root="data", train=True, transform=T.ToTensor(), download=True)
